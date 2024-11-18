e533e18547199c6b0f9032e94e041e0740b3fd45
ROCKX0129 <67377065+ROCKX0129@users.noreply.github.com>
2024-11-18 13:14:23 +0200

    Doge Animation added

--- script_file/doge.gd
+++ script_file/doge.gd
@@ -1,19 +1,24 @@
 extends Area2D
 
 class_name Attack
+@onready var Doge_Ani = $Doge_Ani
 
 var Doge_jumped_high = 0
-var speed = 6
-var Size_speed = 200
+var speed = 4
+var Size_speed = 150
 var d =0
 @onready var player = get_parent().get_node("Player")
 
 func _physics_process(delta: float) -> void:
+	Doge_Ani.play()
 	d += delta
 	global_position = Vector2(
 	cos(d*speed)*Size_speed,
 	sin(d*speed)*Size_speed
 	)+player.global_position
+	if self.position.x-player.global_position.x <0:
+		Doge_Ani.set_flip_h(false)
+	else:
+		Doge_Ani.set_flip_h(true)
+		pass
 	
-	
-	pass
