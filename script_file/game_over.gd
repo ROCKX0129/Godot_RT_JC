67be8870142654ef451b3d9a224b83eae9e440c0
Topi-max <topi.leinonen@edu.turkuamk.fi>
2024-11-28 11:59:24 +0200

    Nyt palauttaa PlayerHP GameOverin jälkeen

--- script_file/game_over.gd
+++ script_file/game_over.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 func _on_play_again_pressed() -> void:
 	Game.Presents = 0 #Nollaa lahjat
+	Game.playerHP = 100 # nollaa HP
 	get_tree().change_scene_to_file("res://Scence_file/world.tscn")
 
 func _on_main_menu_pressed() -> void:
 	Game.Presents = 0 #Nollaa lahjat
+	Game.playerHP = 100 # nollaa HP
 	get_tree().change_scene_to_file("res://Scence_file/main.tscn")
 
 func _ready():
