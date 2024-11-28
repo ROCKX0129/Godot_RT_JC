474185891ed816cf59efb575edda28abca31b4d0
Topi-max <topi.leinonen@edu.turkuamk.fi>
2024-11-28 09:28:24 +0200

    lahjalaskuri lisätty myös gameover sceneen

--- script_file/game_over.gd
+++ script_file/game_over.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 func _on_play_again_pressed() -> void:
+	Game.Presents = 0 #Nollaa lahjat
 	get_tree().change_scene_to_file("res://Scence_file/world.tscn")
 
 func _on_main_menu_pressed() -> void:
+	Game.Presents = 0 #Nollaa lahjat
 	get_tree().change_scene_to_file("res://Scence_file/main.tscn")
+
+func _ready():
+	$CanvasLayer/ColorRect/Lahjalaskuri.text = "Presents collected: " + str(Game.Presents)
